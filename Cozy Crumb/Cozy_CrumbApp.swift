5d625a582d8569b96349168f06edb866488137ea
Noah Klimczuk <noahklimczuk2001@gmail.com>
2026-08-15 01:40:53 -0400

    Initial Commit

---
+++ Cozy Crumb/Cozy_CrumbApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cozy_CrumbApp.swift
+//  Cozy Crumb
+//
+//  Created by Noah Klimczuk on 2026-08-15.
+//
+
+import SwiftUI
+
+@main
+struct Cozy_CrumbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
